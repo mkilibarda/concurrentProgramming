f92a6e4173a7c95310f3fd23b1b67e7baeeeffa1
Travis <mcdts002@mymail.unisa.edu.au>
2017-03-27 14:24:33 +1030

    Some more comments added to code + hopefully the .docx report was added

--- src/snakeGame/Server.java
+++ src/snakeGame/Server.java
@@ -19,14 +19,16 @@ public class Server {
             thread.start();
         }
     }
-
+    
+    //to place the threads back into the queue to be reused
     public synchronized void  execute(Runnable task) throws Exception{
         if(this.isStopped) throw
             new IllegalStateException("Server is stopped");
 
         this.taskQueue.enqueue(task);
     }
-
+    
+    //use this code when time is finished for players to make move
     public synchronized void stop(){
         this.isStopped = true;
         for(ThreadPool thread : threads){

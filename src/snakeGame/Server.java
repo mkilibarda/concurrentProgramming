b0fb52da81eef7eb215169d63cf3d12f78ae56aa
Daniel <Daniel@10.233.101.108>
2017-04-24 18:41:44 +0930

    Updated Server to create a GameScreen and 2 Snake threads.

--- src/snakeGame/Server.java
+++ src/snakeGame/Server.java
@@ -1,11 +1,24 @@
 package snakeGame;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Server {
 	//TODO: THREADPOOL FOR AI
 	//TODO: THREADPOOL FOR PLAYERS
 	//TODO: HANDLE GAME SCREEN
 	
+
+	// movement schemes
+	private int[] arrowKeys = new int[] { 38, 40, 37, 39 };
+	private int[] wasd = new int[] { 87, 83, 65, 68 };
+	
+	public Server() {
+		GameScreen ui = new GameScreen();
+		Thread thread1 = new Thread(new Snake( ui, arrowKeys, 1));
+		Thread thread2 = new Thread(new Snake(ui, wasd, 2));
+		thread1.start();
+		thread2.start();
+	}
+
+	public static void main(String[] args) {
+		Server server = new Server();
+	}
 }
--- src/snakeGame/Snake.java
+++ src/snakeGame/Snake.java
@@ -1,13 +1,79 @@
 package snakeGame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Snake implements Runnable{
+
+public class Snake implements KeyListener, Runnable {
+//	Server server;
+	GameScreen ui;
+	int[] keys;
+	int snake_num;
 	
-
-	public Snake(){
+	// KEYS MAP
+	private int direction = -1;
+	private int next_direction = -1;
+	
+	public final static int UP = 0;
+	public final static int DOWN = 1;
+	public final static int LEFT = 2;
+	public final static int RIGHT = 3;
+	
+	public Snake(GameScreen ui, int[] keys, int snake_num) {
+//		this.server = server;
+		this.ui = ui;
+		this.keys = keys;
+		this.snake_num = snake_num;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName() + " thread started");
+		ui.getMainFrame().addKeyListener(this);
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int code = e.getKeyCode();
+		// System.out.println(code);
+		// Dimension dim;
+		// System.out.println("Key pressed" + ke.toString());
+		
+		// paused = false;
+		
+		if (code == keys[0]) {
+			if (direction != DOWN) {
+				next_direction = UP;
+				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
+			}
+			
+		} else if (code == keys[1]) {
+			if (direction != UP) {
+				next_direction = DOWN;
+				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
+			}
+		} else if (code == keys[2]) {
+			if (direction != RIGHT) {
+				next_direction = LEFT;
+				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
+			}
+		} else if (code == keys[3]) {
+			if (direction != LEFT) {
+				next_direction = RIGHT;
+				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
+			}
+			
+		}
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
-
-	public void run(){
+	
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }

52b2c7d05faa3ce95bdcfab38813e342acecfdfd
Daniel <Daniel@10.233.127.160>
2017-05-01 09:33:54 +0930

    Anthony created Buffer class but could not commit

---
+++ src/snakeGame/Buffer.java
@@ -0,0 +1,33 @@
+
+package snakeGame;
+
+public class Buffer {
+	
+	private String currentDirection = "";
+	
+	public Buffer() {
+		
+	}
+	
+	void goUp() {
+		currentDirection = "up";
+	}
+	
+	void goLeft() {
+		currentDirection = "up";
+	}
+	
+	void goDown() {
+		currentDirection = "up";
+	}
+	
+	void goRight() {
+		currentDirection = "up";
+	}
+	
+	String getcurrentDirection() {
+		return currentDirection;
+		
+	}
+	
+}

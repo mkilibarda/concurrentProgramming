131906b71b4d1961bfd32ebe5b766a40f116357e
Travis <mcdts002@mymail.unisa.edu.au>
2017-05-30 13:51:27 +0930

    abc

--- src/snakeGame/Server.java
+++ src/snakeGame/Server.java
@@ -9,19 +9,18 @@ import java.util.TimerTask;
 
 public class Server {
 	public CellList gameScreen = new CellList(100, 100);
-	List<Buffer> playerBuffer = new ArrayList<Buffer>();
 	
 	// the amount of real players
 	int realPlayers = 4;
 //	private Thread[] currentRealPlayers = new Thread[4];
 	// The amount of AI Players
 	int AIPlayers;
-	List<SnakeAI> comp = new ArrayList<SnakeAI>();
-	List<Worker> workers = new ArrayList<Worker>();
+	List<SnakeAI> aiList = new ArrayList<SnakeAI>();
 	
 	// ThreadPool for the AI snakes
 	ThreadPool aiPool;
-	ThreadPool workerPool = new ThreadPool(4,4);
+	// ThreadPool for the workers
+	ThreadPool compPool;
 	
 	// movement schemes
 	// in order of UP / DOWN / LEFT / RIGHT.
@@ -51,37 +50,11 @@ public class Server {
 	public void startGame(int AIPlayers) {
 		createSnakeAI(AIPlayers);
 		gameW.showGameWindow();
-		
-		// Every second allow the AI to make a decision on the direction to move
-		Timer t = new Timer();
-		t.schedule(new TimerTask() {
-			public void run() {
-				try {
-					for (int i = 0; i < comp.size(); i++) {
-						aiPool.execute(comp.get(i));
-					}
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-				try {
-					for (int i = 0; i < workers.size(); i++) {
-						workerPool.execute(workers.get(i));
-					}
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}, 0, 1000);
 	}
 	
 	public void addSnakePlayer(int playerNumber) {
-		Buffer playerBuf = new Buffer(1);
-		playerBuffer.add(playerBuf);
-		Snake player = new SnakePlayer(gameW, playerNumber, keyschemas[playerNumber-1],playerBuf);
+		Snake player = new SnakePlayer(gameW, playerNumber, keyschemas[playerNumber-1]);
 		Thread snakePlayer = new Thread(player);
-		workers.add(new Worker(playerBuf));
 //		currentRealPlayers[playerNumber-1] = snakePlayer;
 		snakePlayer.start();
 	}
@@ -90,16 +63,29 @@ public class Server {
 	 * Method to create all snakes and get them running.
 	 */
 	public void createSnakeAI(int AIPlayers) {
+		compPool = new ThreadPool(4, AIPlayers);
 		
 		// creates and populates a thread pool for AI, as 100 running threads for AI would be too much overhead,
 		// so simulating this with a pool that does 10 at a time is acceptable.
 		aiPool = new ThreadPool(10, AIPlayers);
 		// thread numbers start at 5 to accommodate for the (currently) 4 real-players
 		for (int i = realPlayers; i < AIPlayers + realPlayers; i++) {
-			comp.add(new SnakeAI(gameW, i + 1));
+			aiList.add(new SnakeAI(gameW, i + 1));
 		}
-		
-		
+		// Every second allow the AI to make a decision on the direction to move
+		Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			public void run() {
+				try {
+					for (int i = 0; i < aiList.size(); i++) {
+						aiPool.execute(aiList.get(i));
+					}
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}, 0, 1000);
 	}
 	
 	/**
--- src/snakeGame/SnakePlayer.java
+++ src/snakeGame/SnakePlayer.java
@@ -8,19 +8,16 @@ public class SnakePlayer extends Snake implements KeyListener {
 	//button press array
 	int[] keys;
 	
-	private Buffer buffer;
-	
 	
 	/*
 	 * Constructor for the Snake Player. Need to have the Gamescreen,
 	 * the snake number(for testing Purpose) and the keys it will use
 	 * to move the snake.
 	 */
-	public SnakePlayer(gameWindow gameW, int snake_num, int[] keys, Buffer playerBuffer) {
+	public SnakePlayer(gameWindow gameW, int snake_num, int[] keys) {
 		super(gameW, snake_num);
 		this.keys = keys;
 		// TODO Auto-generated constructor stub
-		buffer = playerBuffer;
 	}
 	
 	/*
@@ -43,9 +40,7 @@ public class SnakePlayer extends Snake implements KeyListener {
 		
 		//if key press is Up and currently no facing down then move snake up
 		if (code == keys[0]) {
-			
 			if (direction != DOWN) {
-				buffer.append(code);
 				next_direction = UP;
 				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
 			}
@@ -53,7 +48,6 @@ public class SnakePlayer extends Snake implements KeyListener {
 			//if key press is Down and current direction is not up then move down
 		} else if (code == keys[1]) {
 			if (direction != UP) {
-				buffer.append(code);
 				next_direction = DOWN;
 				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
 			}
@@ -61,7 +55,6 @@ public class SnakePlayer extends Snake implements KeyListener {
 			//If key press is left and snake not facing right then move left
 		} else if (code == keys[2]) {
 			if (direction != RIGHT) {
-				buffer.append(code);
 				next_direction = LEFT;
 				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
 			}
@@ -69,7 +62,6 @@ public class SnakePlayer extends Snake implements KeyListener {
 			//If key press is right and snake not facing left then move right
 		} else if (code == keys[3]) {
 			if (direction != LEFT) {
-				buffer.append(code);
 				next_direction = RIGHT;
 				System.out.printf("Thread: snake_" + this.snake_num + " === " + next_direction + "\n");
 			}
